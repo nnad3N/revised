bffd4a1e9a90a84d78ddacc0f04c9dd8048ba510
Damian Struk <damianstruk@outlook.com>
2023-10-19 12:26:44 +0200

    Hero + Nav finished

---
+++ src/components/Logo.tsx
@@ -0,0 +1,49 @@
+type FillOptions = "light" | "dark" | "accent";
+
+interface Props {
+  size: number;
+  bodyFill?: FillOptions;
+  dotFill?: FillOptions;
+  className?: string;
+}
+
+const getFillStyle = (fillOption: FillOptions) => {
+  if (fillOption === "light") return "fill-neutral-100";
+  if (fillOption === "accent") return "fill-accent";
+  return "fill-primary";
+};
+
+const Logo: React.FC<Props> = ({
+  size,
+  bodyFill = "dark",
+  dotFill = "accent",
+  className = "",
+}) => {
+  const logoSize = `${size / 16}rem`;
+
+  return (
+    <svg
+      className={`${getFillStyle(bodyFill)} [&>circle]:${getFillStyle(
+        dotFill,
+      )} ${className}`}
+      style={{
+        height: logoSize,
+        width: logoSize,
+      }}
+      viewBox="0 0 101 101"
+    >
+      <g transform="matrix(1,0,0,1,-542,-115)">
+        <g transform="matrix(1,0,0,1,-2.71267,-147.388)">
+          <g transform="matrix(1.36095,0,0,1.36095,273.978,-305.007)">
+            <circle cx="245.143" cy="463.329" r="9.185"></circle>
+          </g>
+          <g transform="matrix(1,0,0,1,524.139,-139.618)">
+            <path d="M108.466,402.68C115.365,402.68 120.966,408.281 120.966,415.18C120.966,422.079 115.365,427.68 108.466,427.68L64.716,427.689C54.361,427.689 45.966,436.084 45.966,446.439L45.966,502.68L20.966,502.68L20.966,446.43C20.966,422.268 40.554,402.68 64.716,402.68L108.466,402.68Z"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Logo;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      display: ["Rowdies"],
+    },
+    extend: {
+      colors: {
+        primary: "rgb(34, 34, 36)",
+        accent: "rgb(223, 102, 74)",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@headlessui/tailwindcss")],
+};

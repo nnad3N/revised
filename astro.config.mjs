4fe3ad12997debc6bc415c779d95acf2170cefdd
Damian Struk <damianstruk@outlook.com>
2023-10-27 14:22:46 +0200

    Updated images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  experimental: {
+    devOverlay: false,
+  },
 });

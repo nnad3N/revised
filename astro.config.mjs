6cb68ff186a4330966b0f5991736bb4a565f36ab
Damian Struk <damianstruk@outlook.com>
2024-01-29 22:05:04 +0100

    Revert "Added test cloudflare pages integration"
    This reverts commit 07d481da0ea20b013792bd7e5961cf42f1f83485.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,11 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: cloudflare({
-    mode: "directory",
-    runtime: {
-      mode: "local",
-      type: "pages",
-      bindings: {
-        EMAIL_RECEIVERS: {
-          type: "var",
-          value: "nnad3n@outlook.com",
-        },
-      },
-    },
-  }),
   integrations: [tailwind(), solidJs()],
+  experimental: {
+    devOverlay: false,
+  },
 });

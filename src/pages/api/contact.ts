2035dbd83b4dee4da1f6b7f0319c92a9a94d9a42
Damian Struk <damianstruk@outlook.com>
2024-01-29 21:16:56 +0100

    Better debug

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,5 +1,14 @@
 import type { CloudflareAPIRoute } from "@/types";
 
-export const GET: CloudflareAPIRoute = (ctx) => {
-  return new Response(JSON.stringify(ctx), { status: 200 });
+export const GET: CloudflareAPIRoute = ({ request, env }) => {
+  console.log(request.cf);
+
+  // Do something with the data, then return a success response
+  return new Response(
+    JSON.stringify({
+      cf: request.cf,
+      env: env ?? "no env",
+    }),
+    { status: 200 },
+  );
 };

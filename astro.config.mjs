4731f2be662ba590affacbb0f28975be530d38ae
Damian Struk <damianstruk@outlook.com>
2026-07-11 18:35:09 +0200

    add proper font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Newsreader",
+      cssVariable: "--font-newsreader",
+      fallbacks: ["serif"],
+      styles: ["normal", "italic"],
+      weights: ["200 800"],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
